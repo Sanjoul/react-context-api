f7c0878cac92da6f9a853951f2834af0534e6453
Sanjoul Bhusal <bsanjoul@gmail.com>
2024-06-10 12:37:18 +0545

    feat(add to cart)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Item from "./components/Item";
+import Cart from "./components/Cart";
+import React from "react";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Item name="Samsung" price={10000} />
+      <Item name="Pendrive" price={4000} />
+      <Item name="Mobile" price={35000} />
+      <Cart />
     </div>
   );
-}
+};
 
 export default App;
